f816e5f9c38499e04045cb31a96803e394aa6676
Parth <parthchhetri02@gmail.com>
2025-11-20 10:41:43 +0545

    wow

--- aayushpasal.java
+++ aayushpasal.java
@@ -1,4 +1,10 @@
 
+/**
+ * Write a description of class h here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
 import java.util.Scanner;
 
 public class aayushpasal
@@ -13,10 +19,5 @@ public class aayushpasal
     double f=d*n;
     System.out.println("Total price of tablets are : "+f);
     System.out.println("Welcome to Aushadhi Pasal \n We have a total of "+n+" number of tablets with NRP cost as: "+f+ " We are always on stock \n Please visit again");
-    
-    
-    
-    
-    
 }
 }
